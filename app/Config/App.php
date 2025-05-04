849a85f02f0b1e7b57d5b9f86a556a0127c6dbcc
Apik Apikk <mmuwaffiqulihsan@gmail.com>
2025-05-04 19:46:00 +0700

    u

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost:8080/';
+	public $baseURL = 'https://toys-production-35c7.up.railway.app/';
 
 	/**
 	 * --------------------------------------------------------------------------
