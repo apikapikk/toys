a585014cd412c69f3899a6931d59cfa898cadd32
Apik Apikk <mmuwaffiqulihsan@gmail.com>
2025-05-04 20:15:26 +0700

    u

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -25,49 +25,40 @@ class Auth extends BaseController
 
     public function proses()
     {
+        // jika methodnya post lanjut proses 
         if ($this->request->getMethod() == 'post') {
-            // Validasi form input
             $rules = $this->validate([
                 'username' => 'required|alpha_numeric',
                 'password' => 'required'
             ]);
-
-            // Jika validasi gagal, kembalikan error
-            if (!$rules) {
+            if (!($rules)) {
                 $respon = [
                     'validasi' => false,
-                    'error' => [
-                        'username' => $this->validator->getError('username'),
-                        'password' => $this->validator->getError('password')
-                    ]
+                    'error'  => $this->validator->getErrors()
                 ];
-                return $this->response->setJSON($respon);
             } else {
-                // Ambil user berdasarkan username yang dimasukkan
                 $user = $this->userModel->getUser($this->request->getPost('username', FILTER_SANITIZE_STRING));
-
+                // var_dump($user); die;
+                // jika username tidak terdaftar 
                 if (empty($user)) {
                     $respon = [
                         'validasi' => true,
                         'error' => ['username' => 'Username tidak terdaftar!']
                     ];
-                    return $this->response->setJSON($respon);
                 } else {
-                    // Verifikasi password
-                    if (!password_verify($this->request->getPost('password'), $user->password)) {
+                    // jika password tidak sama
+                    if (!verifikasi_password($this->request->getPost('password'), $user->password)) {
                         $respon = [
                             'validasi' => true,
                             'error' => ['password' => 'Password tidak sesuai!']
                         ];
-                        return $this->response->setJSON($respon);
                     } else {
-                        // Login sukses, set session user
+                        // login sukses set session user
                         $data = [
                             'login' => true,
                             'id'    => $user->id,
                         ];
-                        $this->session->set($data); // Set session
-
+                        $this->session->set($data); // set session
                         $respon = [
                             'validasi' => true,
                             'sukses' => true,
@@ -75,45 +66,40 @@ class Auth extends BaseController
                             'pesan' => 'Login berhasil!',
                             'roleId' => $user->id_role
                         ];
-                        return $this->response->setJSON($respon);
                     }
                 }
+                // var_dump($user);
             }
+            return $this->response->setJSON($respon);
         }
     }
 
     public function lupaPassword()
     {
+        // jika request methodnya get tampilkan form reset password 
         if ($this->request->getMethod() == 'get') {
             echo view('auth/lupa-password', ['title' => 'Lupa Password']);
         } else {
-            // Validasi email
             $rules = $this->validate([
                 'email' => 'required|valid_email',
             ]);
-
-            if (!$rules) {
+            if (!($rules)) {
                 $respon = [
                     'validasi' => false,
                     'error' => $this->validator->getErrors()
                 ];
-                return $this->response->setJSON($respon);
             } else {
-                // Ambil user berdasarkan email
                 $user = $this->userModel->getUser($this->request->getPost('email', FILTER_SANITIZE_EMAIL));
-
+                // jika email tidak terdaftar 
                 if (empty($user)) {
                     $respon = [
                         'validasi' => true,
                         'error' => ['email' => 'Email tidak terdaftar!']
                     ];
-                    return $this->response->setJSON($respon);
                 } else {
-                    // Generate token untuk reset password
                     $token = bin2hex(random_bytes(32));
                     $this->userModel->update($user->id, ['token' => $token]);
-                    $kirimEmail = $this->_kirimEmail($user->email, 'reset', $token);
-
+                    $kirimEmail = $this->_kirimEmail($user->email, 'reset', $token); // kirik email
                     if ($kirimEmail) {
                         $respon = [
                             'validasi' => true,
@@ -128,9 +114,9 @@ class Auth extends BaseController
                             'pesan' => 'Tidak dapat mengirim email, server sedang gangguan!'
                         ];
                     }
-                    return $this->response->setJSON($respon);
                 }
             }
+            return $this->response->setJSON($respon);
         }
     }
 
@@ -143,34 +129,29 @@ class Auth extends BaseController
     public function gantiPassword()
     {
         if ($this->request->getMethod() == 'post') {
-            // Validasi password
             $rules = $this->validate([
                 'password' => 'required|min_length[6]|matches[konfirmasi_password]',
                 'konfirmasi_password' => 'matches[password]'
             ]);
-
             if (!$rules) {
+                // jika validasi form gagal
                 $respon = [
                     'validasi' => false,
                     'error' => $this->validator->getErrors()
                 ];
-                return $this->response->setJSON($respon);
             } else {
-                // Ambil user berdasarkan token
                 $user = $this->userModel->getToken($this->request->getPost('kode'));
-
                 if (empty($user)) {
                     $respon = [
                         'validasi' => true,
                         'error' => ['token' => 'Token tidak valid, silahkan request link reset password kembali!']
                     ];
-                    return $this->response->setJSON($respon);
                 } else {
-                    // Update password
+                    // update password
                     $password = buat_password($this->request->getPost('konfirmasi_password'));
                     try {
                         $this->userModel->update($user->id, ['password' => $password, 'token' => null]);
-                        $this->_kirimEmail($user->email, 'ganti');
+                        $this->_kirimEmail($user->email, 'ganti'); // kirik email
                         $respon = [
                             'validasi' => true,
                             'sukses' => true,
@@ -181,18 +162,47 @@ class Auth extends BaseController
                         $respon = [
                             'validasi' => true,
                             'sukses' => false,
-                            'pesan' => 'Gagal mengubah password, coba lagi nanti.'
+                            'pesan' => 'Gagal mengirim email!'
                         ];
                     }
-                    return $this->response->setJSON($respon);
                 }
             }
+            return $this->response->setJSON($respon);
         }
     }
 
+    /**
+     * Kirim email untuk verifikasi akun
+     * @param string $email email tujuan
+     * @param string $token token verifikasi
+     */
+    public function verifikasiAkun(string $email, string $token)
+    {
+        return $this->_kirimEmail($email, 'verifikasi', $token);
+    }
+
+    /**
+     * Verifikasi akun baru
+     */
+    public function verifikasi()
+    {
+        $token = $this->request->getGet('token', FILTER_SANITIZE_STRING);
+        $user = $this->userModel->getToken($token);
+        if (empty($user)) {
+            echo 'Token tidak valid :(';
+        } else {
+            $this->userModel->update($user->id, ['status' => 1, 'token' => '']);
+            echo 'Selamat akun kamu sudah aktif silahkan login :)';
+            echo '<br/><br/> <a href="' . base_url() . '">LOGIN</a>';
+        }
+    }
+
+    /**
+     * @return bool true jika kirim email berhasil, false jika gagal
+     */
     private function _kirimEmail($tujuan, $tipe = 'reset', $token = null): bool
     {
-        $user = $this->userModel->getUser($tujuan);
+        $user = $this->userModel->getUser($tujuan); // ambil data user sesuai email ($tujuan)
         if (empty($user)) {
             return false;
         }
@@ -211,6 +221,10 @@ class Auth extends BaseController
         } elseif ($tipe == 'ganti') {
             $subjek = 'Ubah Password';
             $pesan = 'Password berhasil di ubah, silahkan login kembali :)';
+        } elseif ($tipe == 'notif') {
+            $subjek = 'Konfirmasi Perubahan Email';
+            $pesan = 'Anda telah melakukan permintaan untuk mengubah email. Untuk melanjutkan silahkan<br/><br/> <a href="' . base_url("auth/konfirmasi?token={$token}") . '">KLIK DISINI</a><br/>';
+            $pesan .= 'Tapi jika Anda tidak pernah meminta proses ini, maka kami berharap Anda mengabaikan email ini.';
         } elseif ($tipe == 'verifikasi') {
             $subjek = 'Verifikasi Akun';
             $pesan = 'Silahkan konfirmasi email Anda<br/><br/> <a href="' . base_url("auth/verifikasi?token={$token}") . '">KLIK DISINI</a><br/>';
@@ -235,7 +249,7 @@ class Auth extends BaseController
 
     public function logout()
     {
-        session()->remove(['login', 'id']);
-        return redirect()->to(base_url())->with('message', 'Anda telah berhasil logout.');
+        session()->remove(['login', 'id']); // hapus session login dan id
+        return redirect()->to(base_url());
     }
 }
